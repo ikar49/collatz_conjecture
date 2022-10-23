72ee6bc071f5146ededeb629be231efd16a22485
IkaR49 <ikar49@pm.me>
2022-10-23 09:55:27 +0300

    refactor: Prepare for impl for some NonZero* types

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,17 @@
-use std::io;
+use std::io::{self, Error, ErrorKind::Other};
 use std::num::NonZeroU128;
 
-#[derive(Debug, Clone, Copy)]
-struct CollatzIterator(u128);
-
-impl CollatzIterator {
-    fn new(n: NonZeroU128) -> Self {
-        Self(n.get())
-    }
-}
-
-impl Iterator for CollatzIterator {
-    type Item = NonZeroU128;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        let result = NonZeroU128::new(self.0.clone());
-
-        if &self.0 % 2 == 0 {
-            self.0 = self.0 / 2;
-        } else {
-            self.0 = self.0.checked_mul(3)
-                            // Записываем MAX, чтобы
-                           .unwrap_or(u128::MAX)
-                            // вызвать второе переполнение при сложении, чтобы
-                           .checked_add(1)
-                            // точно записать 0 в self
-                           .unwrap_or(0);
-        }
-
-        result
-    }
-}
+use collatz_conjecture::*;
 
 fn main() -> io::Result<()> {
-    let mut collatz_iter = CollatzIterator::new(NonZeroU128::new(
+    let mut collatz_iter = NonZeroU128::new(
         1024_u128.pow(12_u32) * 255 // + 1 // with +1 will overflow on first step
-    ).unwrap());
+    ).unwrap().collatz_iter();
     println!("Test: {:?}", collatz_iter);
+
     loop {
         let n = collatz_iter.next()
-            .ok_or(io::Error::new(io::ErrorKind::Other, "Overflow u128!"))?;
+            .ok_or(Error::new(Other, "Overflow u128!"))?;
 
         println!("{}", n);
         if n == NonZeroU128::new(1).unwrap() {
