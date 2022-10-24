365623ee30728df75f5269ec31b0f505e45d9a4f
IkaR49 <ikar49@pm.me>
2022-10-24 23:19:35 +0300

    feat: Введены фичи для включения реализации для разных примитивных типов

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,3 @@
-pub mod prelude;
-
 pub trait CollatzIterator {
     type Iterator;
     fn collatz_iter(&self) -> Self::Iterator;
@@ -8,50 +6,64 @@ pub trait CollatzIterator {
 #[macro_use]
 mod r#macro;
 
+#[cfg(feature = "u8")]
 pub mod r#u8 {
     collatz!(CollatzIterU8(u8) for NonZeroU8);
 }
 
+#[cfg(feature = "u16")]
 pub mod r#u16 {
     collatz!(CollatzIterU16(u16) for NonZeroU16);
 }
 
+#[cfg(feature = "u32")]
 pub mod r#u32 {
     collatz!(CollatzIterU32(u32) for NonZeroU32);
 }
 
+#[cfg(feature = "u64")]
 pub mod r#u64 {
     collatz!(CollatzIterU64(u64) for NonZeroU64);
 }
 
+#[cfg(feature = "u128")]
 pub mod r#u128 {
     collatz!(CollatzIterU128(u128) for NonZeroU128);
 }
 
+#[cfg(feature = "usize")]
 pub mod r#usize {
     collatz!(CollatzIterUsize(usize) for NonZeroUsize);
 }
 
+#[cfg(feature = "i8")]
 pub mod r#i8 {
     collatz!(CollatzIterI8(i8) for NonZeroI8);
 }
 
+#[cfg(feature = "i16")]
 pub mod r#i16 {
     collatz!(CollatzIterI16(i16) for NonZeroI16);
 }
 
+#[cfg(feature = "i32")]
 pub mod r#i32 {
     collatz!(CollatzIterI32(i32) for NonZeroI32);
 }
 
+#[cfg(feature = "i64")]
 pub mod r#i64 {
     collatz!(CollatzIterI64(i64) for NonZeroI64);
 }
 
+#[cfg(feature = "i128")]
 pub mod r#i128 {
     collatz!(CollatzIterI128(i128) for NonZeroI128);
 }
 
+#[cfg(feature = "isize")]
 pub mod r#isize {
     collatz!(CollatzIterIsize(isize) for NonZeroIsize);
 }
+
+pub mod prelude;
--- src/prelude.rs
+++ src/prelude.rs
@@ -1,37 +1,37 @@
 pub use crate::CollatzIterator;
 
+#[cfg(feature = "u8")]
 pub use std::num::NonZeroU8;
-//pub use crate::r#u8::CollatzIterator as CollatzIteratorU8;
 
+#[cfg(feature = "u16")]
 pub use std::num::NonZeroU16;
-//pub use crate::r#u16::CollatzIterator as CollatzIteratorU16;
 
+#[cfg(feature = "u32")]
 pub use std::num::NonZeroU32;
-//pub use crate::r#u32::CollatzIterator as CollatzIteratorU32;
 
+#[cfg(feature = "u64")]
 pub use std::num::NonZeroU64;
-//pub use crate::r#u64::CollatzIterator as CollatzIteratorU64;
 
+#[cfg(feature = "u128")]
 pub use std::num::NonZeroU128;
-//pub use crate::r#u128::CollatzIterator as CollatzIteratorU128;
 
+#[cfg(feature = "usize")]
 pub use std::num::NonZeroUsize;
-//pub use crate::r#usize::CollatzIterator as CollatzIteratorUsize;
 
+#[cfg(feature = "i8")]
 pub use std::num::NonZeroI8;
-//pub use crate::r#i8::CollatzIterator as CollatzIteratorI8;
 
+#[cfg(feature = "i16")]
 pub use std::num::NonZeroI16;
-//pub use crate::r#i16::CollatzIterator as CollatzIteratorI16;
 
+#[cfg(feature = "i32")]
 pub use std::num::NonZeroI32;
-//pub use crate::r#i32::CollatzIterator as CollatzIteratorI32;
 
+#[cfg(feature = "i64")]
 pub use std::num::NonZeroI64;
-//pub use crate::r#i64::CollatzIterator as CollatzIteratorI64;
 
+#[cfg(feature = "i128")]
 pub use std::num::NonZeroI128;
-//pub use crate::r#i128::CollatzIterator as CollatzIteratorI128;
 
+#[cfg(feature = "isize")]
 pub use std::num::NonZeroIsize;
-//pub use crate::r#isize::CollatzIterator as CollatzIteratorIsize;
